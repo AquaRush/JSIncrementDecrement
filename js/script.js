4ec886b63c7f9e741128e84704c931f356075b49
AquaRush <george.mihaylov96@gmail.com>
2023-12-26 18:28:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Variables to target HTML elements
+const btnCounter = document.getElementById("counter");
+const btnIncrement = document.getElementById("btn-increment");
+const btnDecrement = document.getElementById("btn-decrement");
+const btnReset = document.getElementById("btn-reset");
+let counter = 0;
+let interval;
+
+// Decrease counter button
+function numDecrease() {
+    counter--;
+    btnCounter.innerHTML = counter;
+}
+
+// Increase counter button
+function numIncrease() {
+    counter++;
+    btnCounter.innerHTML = counter;
+}
+
+// Reset counter button
+function resetCounter() {
+    counter = 0;
+    btnCounter.innerHTML = counter;
+}
+
+numDecrease();
+numIncrease();
+resetCounter();
+
+
+// Decrement on click, Decrement faster on mouse hold, stop Decrementing on mouse release
+btnDecrement.addEventListener("click", numDecrease);
+btnDecrement.addEventListener("mousedown", function() {
+    interval = setInterval(numDecrease, 80);
+})
+btnDecrement.addEventListener("mouseup", function () {
+    clearInterval(interval);
+})
+
+// Increment on click, increment faster on mouse hold, stop incrementing on mouse release
+btnIncrement.addEventListener("click", numIncrease);
+btnIncrement.addEventListener("mousedown", function() {
+    interval = setInterval(numIncrease, 80);
+})
+btnIncrement.addEventListener("mouseup", function () {
+    clearInterval(interval);
+})
+
+// Reset counter on click
+btnReset.addEventListener("click", resetCounter);
+
